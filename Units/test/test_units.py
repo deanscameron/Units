a6211ba95c89aa4a572f6d6de1177e23d5cd8017
Cameron Deans <cameron.deans.14@ucl.ac.uk>
2015-01-15 08:39:49 +0000

    edited tests file to represent the function names used

--- Units/test/test_units.py
+++ Units/test/test_units.py
@@ -1,9 +1,8 @@
 from nose.tools import assert_true, assert_raises
 
-# Aim to pass the following tests from the exercise
 def test_to_pass():
     assert_true(5*meters == 0.005*kilometers)
-    assert_true((60*seconds).to(minutes).value==1)
-    assert_true((60*seconds).to(minutes).unit==minutes)
-    with assert_raises(IncompatibleUnitsError):
+    assert_true((60*seconds).to_unit(minutes).coeff==1)
+    assert_true((60*seconds).to_unit(minutes).unit==minutes)
+    with assert_raises(TypeError):
         5*meters+2*seconds
