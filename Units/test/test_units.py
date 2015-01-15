6c372e52b6f39b0a3dbff862bfb72efe8196eda6
Cameron Deans <cameron.deans.14@ucl.ac.uk>
2015-01-15 08:49:45 +0000

    added some units to test_units, corrected spelling

--- Units/test/test_units.py
+++ Units/test/test_units.py
@@ -1,8 +1,72 @@
 from nose.tools import assert_true, assert_raises
 
+class Unit(object):
+    def __init__(self, unit_type, unit_scale):
+        # unit_type (eg. time) to define similar units
+        # unit_scale (eg. 60 for minute) to compute in similar units
+        # would allow second=Unit("time", 1)
+        self.unit_type=unit_type
+        self.unit_scale=unit_scale
+		 
+    def __rmul__(self, constant):
+        # multiplies Units by a constant
+        if type(constant) == int and type(self)==Unit:		
+            return Term(self, constant)
+        elif type(constant) == float and type(self)==Unit:
+            return Term(self, constant)
+        else:
+            raise TypeError("Units can only be multiplied by int or float")
+
+class Term(object):
+    def __init__(self, unit, coeff):
+        # just a number times a unit	
+        self.unit=unit
+        self.coeff=coeff
+		
+    def __add__(self, other):
+        # adds Terms of the same unit_type
+        if other.unit.unit_type == self.unit.unit_type:
+            new_coeff=self.coeff+other.coeff*(other.unit.unit_scale/float(self.unit.unit_scale))
+        else:
+            raise TypeError("Cannot sum terms with different types of unit")		
+        return Term(unit, new_coeff)
+		
+    def __rmul__(self, constant):
+        # multiplies Terms by a constant
+        if type(constant) == int and type(self)==Unit:
+            new_coeff=constant*self.coeff		
+            return Term(unit, new_coeff)
+        elif type(constant) == float and type(self)==Unit:
+            new_coeff=constant*self.coeff
+            return Term(unit, new_coeff)
+        else:
+            raise TypeError("Terms can only be multiplied by int or float")
+		
+    def __eq__(self, other):
+        # compares Terms of same unit_type
+        if other.unit.unit_type != self.unit.unit_type:
+            raise TypeError("Terms with different unit types cannot be equal")
+        elif other.coeff*other.unit.unit_scale == self.coeff*self.unit.unit_scale:
+            return True
+        else:
+            return False	
+			
+    def to_unit(self, unit):
+        if self.unit.unit_type != unit.unit_type:
+            raise TypeError("Terms cannot be converted to units of different type")
+        else:
+            new_coeff=self.coeff*(self.unit.unit_scale/float(unit.unit_scale))
+        return Term(unit, new_coeff)
+
+metres=Unit("length", 1)
+kilometres=Unit("length", 1000)
+seconds=Unit("time", 1)
+minutes=Unit("time", 60)
+	
+		
 def test_to_pass():
-    assert_true(5*meters == 0.005*kilometers)
+    assert_true(5*metres == 0.005*kilometres)
     assert_true((60*seconds).to_unit(minutes).coeff==1)
     assert_true((60*seconds).to_unit(minutes).unit==minutes)
     with assert_raises(TypeError):
-        5*meters+2*seconds
+        5*metres+2*seconds
